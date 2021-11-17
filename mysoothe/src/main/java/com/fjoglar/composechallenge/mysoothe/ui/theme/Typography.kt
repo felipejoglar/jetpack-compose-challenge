edcd0caf26a55d012140f610ad8e83a0373d765f
Felipe Joglar <felipejoglar@gmail.com>
2021-11-17 11:26:55 +0100

    Add Material theming

--- mysoothe/src/main/java/com/fjoglar/composechallenge/mysoothe/ui/theme/Typography.kt
+++ mysoothe/src/main/java/com/fjoglar/composechallenge/mysoothe/ui/theme/Typography.kt
@@ -2,27 +2,57 @@ package com.fjoglar.composechallenge.mysoothe.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.fjoglar.composechallenge.mysoothe.R
 
-// Set of Material typography styles to start with
+private val KulimPark = FontFamily(
+    Font(R.font.kulim_park_light, FontWeight.Light),
+    Font(R.font.kulim_park_regular, FontWeight.Normal),
+)
+
+private val Lato = FontFamily(
+    Font(R.font.lato_regular, FontWeight.Normal),
+    Font(R.font.lato_bold, FontWeight.Bold),
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = KulimPark,
+        fontWeight = FontWeight.Light,
+        fontSize = 28.sp,
+        letterSpacing = 1.15.sp,
+    ),
+    h2 = TextStyle(
+        fontFamily = KulimPark,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+        fontSize = 15.sp,
+        letterSpacing = 1.15.sp,
+    ),
+    h3 = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp,
+    ),
+    body1 = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp,
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = Lato,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 1.15.sp,
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = KulimPark,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontSize = 12.sp,
+        letterSpacing = 1.15.sp,
+    ),
 )
