2a1c1ea88c0af394153045dc2d6e7037880c0f8a
Felipe Joglar <felipejoglar@gmail.com>
2021-11-17 11:27:38 +0100

    My Soothe theming (#10)
    * Add drawable resources for light and dark themes
    * Add custom font resources
    * Add Material theming

--- mysoothe/src/main/java/com/fjoglar/composechallenge/mysoothe/ui/theme/Color.kt
+++ mysoothe/src/main/java/com/fjoglar/composechallenge/mysoothe/ui/theme/Color.kt
@@ -2,7 +2,13 @@ package com.fjoglar.composechallenge.mysoothe.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val White = Color(0xFFFFFFFF)
+val White150 = White.copy(alpha = 0.15f)
+val White800 = White.copy(alpha = 0.80f)
+val White850 = White.copy(alpha = 0.85f)
+val Gray900 = Color(0xFF333333)
+val Gray800 = Gray900.copy(alpha = 0.8f)
+val Rust300 = Color(0xFFE1AFAF)
+val Rust600 = Color(0xFF886363)
+val Taupe100 = Color(0xFFF0EAE2)
+val Taupe800 = Color(0xFF655454)
