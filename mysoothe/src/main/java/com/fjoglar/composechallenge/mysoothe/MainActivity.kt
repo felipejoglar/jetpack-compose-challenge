3ace108baf156c592e3e6fcb6b3d897976e1b337
Felipe Joglar <felipejoglar@gmail.com>
2022-03-18 11:10:55 +0100

    [MySoothe] Add transparency to status bar

--- mysoothe/src/main/java/com/fjoglar/composechallenge/mysoothe/MainActivity.kt
+++ mysoothe/src/main/java/com/fjoglar/composechallenge/mysoothe/MainActivity.kt
@@ -3,12 +3,16 @@ package com.fjoglar.composechallenge.mysoothe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import com.fjoglar.composechallenge.mysoothe.ui.components.MySootheTemplate
 import com.fjoglar.composechallenge.mysoothe.ui.screens.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             MySootheTemplate {
                 LoginScreen()
